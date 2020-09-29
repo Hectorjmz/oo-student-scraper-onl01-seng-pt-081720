cb91d914461d5dae1edae42739554dc6fb803474
Hector_Jimenez <hjmontemayor@gmail.com>
2020-09-28 20:19:49 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,22 @@
 require 'open-uri'
+require 'nokogiri'
+require 'json'
 require 'pry'
 
 class Scraper
 
+
+  #is a class method that scrapes the student index page and a 
+  #returns an array of hashes in which each hash represents one student
   def self.scrape_index_page(index_url)
-    
+    Nokogiri::HTML.parse(index_url)
   end
 
   def self.scrape_profile_page(profile_url)
     
   end
+
+  
 
 end
 
