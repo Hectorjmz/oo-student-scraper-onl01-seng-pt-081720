73c5a68507c64872531dbd9b89e4cbd48ba979b4
Hector_Jimenez <hjmontemayor@gmail.com>
2020-09-28 20:29:41 -0400

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
   #is a class method that scrapes the student index page and a 
   #returns an array of hashes in which each hash represents one student
   def self.scrape_index_page(index_url)
-    Nokogiri::HTML.parse(index_url)
+    Nokogiri::HTML.parse(open(index_url))
   end
 
   def self.scrape_profile_page(profile_url)
